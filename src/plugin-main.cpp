070ad3b0dc56417f93e8f3b0f2d86c68f61eed3b
Owen Isenhart <isenhart.owen@gmail.com>
2026-05-14 09:59:03 -0500

    update template headers

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) 2026 Owen Isenhart oisenhart.college@gmail.com
+uhohbs
+Copyright (C) 2026 Owen Isenhart isenhart.owen@gmail.com
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+uhohbs
+Copyright (C) 2026 Owen Isenhart isenhart.owen@gmail.com
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
